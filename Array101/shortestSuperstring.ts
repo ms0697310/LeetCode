594cf229290f869bb715d8db033acc701cf95702
YSS <ms0697310@outlook.com>
2023-02-20 18:58:23 +0800

    unsolved: shortest super string
    https://leetcode.com/problems/find-the-shortest-superstring/

---
+++ Array101/shortestSuperstring.ts
@@ -0,0 +1,6 @@
+import { test } from "../Testing/unitTest";
+test(["alex", "loves", "leetcode"], "alexlovesleetcode", shortestSuperstring);
+test(["catg", "ctaagt", "gcta", "ttca", "atgcatc"], "gctaagttcatgcatc", shortestSuperstring);
+function shortestSuperstring(words: string[]): string {
+    return "";
+}
