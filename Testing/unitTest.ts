50479fd56a838c76fbe4d29987fa40866c688448
YSS <ms0697310@outlook.com>
2023-02-21 15:30:34 +0800

    feat. compareAnswerInplace

--- Testing/unitTest.ts
+++ Testing/unitTest.ts
@@ -4,3 +4,8 @@ export function test(input: any, output: any, resolve: Function) {
 export function compareAnswer(myAns: any, ans: any) {
     console.log(JSON.stringify(myAns) === JSON.stringify(ans), "myAns=", myAns, ", ans=", ans);
 }
+
+export function compareAnswerInplace(input: any, ans: any, resolve: Function) {
+    resolve(input);
+    compareAnswer(input, ans);
+}
